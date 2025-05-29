534bf932a0ebef7f3fcff217a422c459149dd82c
Marnie Nickelson <marnie.nickelson@punchkick.com>
2025-05-28 20:59:59 -0400

    test

--- app/markers/page.tsx
+++ app/markers/page.tsx
@@ -252,7 +252,7 @@ export default function MarkersPage() {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
               </svg>
-              <span>Add New Marker</span>
+              <span>Add a New Marker</span>
             </Link>
           </motion.div>
         </div>
